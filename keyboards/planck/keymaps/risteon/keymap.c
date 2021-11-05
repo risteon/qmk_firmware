db7e77d240e8aaa9f4425cccffd6ecde163507a6
Christoph Rist <c.rist@posteo.de>
2021-11-05 09:38:25 +0100

    Add backtick

--- keyboards/planck/keymaps/risteon/keymap.c
+++ keyboards/planck/keymaps/risteon/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Print |      |      |      |      |  <-  | DOWN |  UP  |  ->  | Ins  |  |   |
+ * |   `  |Print |      |      |      |      |  <-  | DOWN |  UP  |  ->  | Ins  |  |   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |ISO ~ |ISO | | Home | End  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -111,7 +111,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_LOWER] = LAYOUT_planck_grid(
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,       KC_ASTR,       KC_LPRN, KC_RPRN, KC_BSPC,
-    _______, KC_PSCR, _______, _______, _______, _______, KC_LEFT, KC_DOWN,       KC_UP,         KC_RGHT, KC_INS,  KC_PIPE,
+    KC_GRV,  KC_PSCR, _______, _______, _______, _______, KC_LEFT, KC_DOWN,       KC_UP,         KC_RGHT, KC_INS,  KC_PIPE,
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   LSFT(KC_NUHS), LSFT(KC_NUBS), KC_HOME, KC_END,  _______,
     _______, _______, _______, _______, _______, _______, _______, _______,       KC_MNXT,       KC_VOLD, KC_VOLU, KC_MPLY
 ),
