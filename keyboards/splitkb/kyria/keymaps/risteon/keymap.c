6cddca4e9feb8365ff862e10fc20331269219dd8
Christoph Rist <c.rist@posteo.de>
2022-12-29 19:29:44 +0100

    [kyria] Adapt keymap name printed on OLED

--- keyboards/splitkb/kyria/keymaps/risteon/keymap.c
+++ keyboards/splitkb/kyria/keymaps/risteon/keymap.c
@@ -572,7 +572,7 @@ bool oled_task_user(void) {
         // clang-format on
 
         oled_write_P(qmk_logo, false);
-        oled_write_P(PSTR("Keymap: sbmueller\n\n"), false);
+        oled_write_P(PSTR("Keymap: risteon\n\n"), false);
 
         // Host Keyboard Layer Status
         oled_write_P(PSTR("Layer: "), false);
