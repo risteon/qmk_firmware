e878ff19644b4b5e7d4dbc896e0e60e5d111e37e
Christoph Rist <c.rist@posteo.de>
2024-08-05 10:52:18 +0200

    [kyria] Change animation to space saving diff render
    2 sets of 2 animation frames. Render and save only diffs.

--- keyboards/splitkb/kyria/keymaps/risteon/config.h
+++ keyboards/splitkb/kyria/keymaps/risteon/config.h
@@ -22,26 +22,23 @@
 
 #ifdef OLED_ENABLE
 #    define OLED_DISPLAY_128X64
+#    define OLED_TIMEOUT 60000
+#    define OLED_BRIGHTNESS 100
+
+#    define SPLIT_WPM_ENABLE
+#    define WPM_LAUNCH_CONTROL
+#    define WPM_SAMPLE_PERIODS 5
+#    define WPM_SAMPLE_SECONDS 25
 #endif
+
+#define SPLIT_LAYER_STATE_ENABLE
+#define SPLIT_LED_STATE_ENABLE
 
 // Disable TAPPING_FORCE_HOLD only for meta-bksp mod tap
 #define TAPPING_FORCE_HOLD_PER_KEY
 #define TAPPING_TERM 150
 #define PERMISSIVE_HOLD
 #define HOLD_ON_OTHER_KEY_PRESS
-
-#define SPLIT_WPM_ENABLE
-#define SPLIT_LAYER_STATE_ENABLE
-#define SPLIT_LED_STATE_ENABLE
-#define OLED_TIMEOUT 60000
-#define OLED_BRIGHTNESS 100
-
-#define NO_ACTION_MACRO
-#define NO_ACTION_FUNCTION
-
-#define WPM_LAUNCH_CONTROL
-#define WPM_SAMPLE_PERIODS 5
-#define WPM_SAMPLE_SECONDS 25
 
 #define ENCODER_RESOLUTION 2
 
@@ -52,39 +49,19 @@
 // turn off when suspended
 #    define RGB_DISABLE_WHEN_USB_SUSPENDED
 #    define RGBLIGHT_SLEEP
+// #    define RGBLIGHT_EFFECT_BREATHING
+// #    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+// #    define RGBLIGHT_EFFECT_SNAKE
+// #    define RGBLIGHT_EFFECT_KNIGHT
+// #    define RGBLIGHT_EFFECT_CHRISTMAS
+// #    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+// #    define RGBLIGHT_EFFECT_RGB_TEST
+// #    define RGBLIGHT_EFFECT_ALTERNATING
+// #    define RGBLIGHT_EFFECT_TWINKLE
+#    define RGBLIGHT_HUE_STEP 8
+#    define RGBLIGHT_SAT_STEP 8
+#    define RGBLIGHT_VAL_STEP 8
+#    ifndef RGBLIGHT_LIMIT_VAL
+#        define RGBLIGHT_LIMIT_VAL 150
+#    endif
 #endif // RGBLIGHT_ENABLE
-
-#ifdef RGB_MATRIX_ENABLE
-#    define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
-// bug in rgb_matrix.c? slave half does not work well with this
-//#   define RGB_DISABLE_TIMEOUT 12000
-#    define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#    define RGBLIGHT_SLEEP                 // allows us to use rgblight_suspend() and rgblight_wakeup() in keymap.c
-#    define RGBLIGHT_TIMEOUT 300000        // ms
-
-#    define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-#    define RGB_MATRIX_HUE_STEP 8
-#    define RGB_MATRIX_SAT_STEP 8
-#    define RGB_MATRIX_VAL_STEP 8
-#    define RGB_MATRIX_SPD_STEP 10
-
-/* Disable the animations you don't want/need.  You will need to disable a good number of these    *
- * because they take up a lot of space.  Disable until you can successfully compile your firmware. */
-// RGB Matrix Animation modes. Explicitly enabled
-// For full list of effects, see:
-// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
-#    define ENABLE_RGB_MATRIX_SOLID_COLOR
-// #    define ENABLE_RGB_MATRIX_ALPHAS_MODS
-//#    define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-// #    define DISABLE_RGB_MATRIX_GRADIENT_UP_DOWN
-#    define ENABLE_RGB_MATRIX_GRADIENT_LEFT_RIGHT
-// #    define ENABLE_RGB_MATRIX_BREATHING
-// #    define DISABLE_RGB_MATRIX_BREATHING
-// #    define ENABLE_RGB_MATRIX_BAND_SAT
-// #    define ENABLE_RGB_MATRIX_BAND_VAL
-//#    define ENABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-// #    define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-#    define RGB_MATRIX_SOLID_REACTIVE_GRADIENT_MODE
-
-#endif // RGB_MATRIX_ENABLE
--- keyboards/splitkb/kyria/keymaps/risteon/rules.mk
+++ keyboards/splitkb/kyria/keymaps/risteon/rules.mk
@@ -1,21 +1,23 @@
-OLED_ENABLE = yes
-OLED_DRIVER = ssd1306   # Enables the use of OLED displays
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes
-RGB_MATRIX_ENABLE = no
-WPM_ENABLE = yes # Enable words per minute calculation
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix, as it is enabled by default on rev3
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 EXTRAFLAGS += -flto
-CONSOLE_ENABLE = no
-SLEEP_LED_ENABLE = yes
-TERMINAL_ENABLE = no
+
+OLED_ENABLE = yes
+WPM_ENABLE = yes # Enable words per minute calculation
+
+EXTRAKEY_ENABLE = yes
+LTO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
+SLEEP_LED_ENABLE = yes
+
+CONSOLE_ENABLE = no
+TERMINAL_ENABLE = no
 COMMAND_ENABLE = no
 MIDI_ENABLE = no
 AUDIO_ENABLE = no
 UNICODE_ENABLE = no
 KEY_OVERRIDE_ENABLE = no
-LTO_ENABLE = yes
-EXTRAKEY_ENABLE = yes
 MAGIC_ENABLE = no
 SPACE_CADET_ENABLE = no
 GRAVE_ESC_ENABLE = no
