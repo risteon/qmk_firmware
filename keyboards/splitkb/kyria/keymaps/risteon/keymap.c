d6498409543a025da408df4f11886c731a8c1eb9
Christoph Rist <c.rist@posteo.de>
2024-08-20 14:32:26 +0200

    [kyria] Mask out outer row keys

--- keyboards/splitkb/kyria/keymaps/risteon/keymap.c
+++ keyboards/splitkb/kyria/keymaps/risteon/keymap.c
@@ -70,17 +70,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-------------------------------------------.                              ,-------------------------------------------.
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  Bksp  |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |Lalt/Esc|   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
+ * |  L-ALT |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  R-ALT |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * |LShift/[|   Z  |   X  |   C  |   V  |   B  |      |Adjust|  |Adjust|      |   N  |   M  | ,  < | . >  | /  ? |RShift/]|
+ * |        |   Z  |   X  |   C  |   V  |   B  |      |Adjust|  |Adjust|      |   N  |   M  | ,  < | . >  | /  ? |        |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
  *                     | Mute |Ctrl(Esc)|Lower |Shift |      |  |      |Space |Raise |GUI/Bk|      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB,  KC_Q, KC_W, KC_E,     KC_R,    KC_T,                                       KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,    KC_BSPC,
-     ALT_ESC, KC_A, KC_S, KC_D,     KC_F,    KC_G,                                       KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, ALT_QUOT,
-     KC_MINS, KC_Z, KC_X, KC_C,     KC_V,    KC_B,  _______,  ADJUST,  ADJUST, _______,  KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_ENT,
+     XXXXXXX,  KC_Q, KC_W, KC_E,     KC_R,    KC_T,                                       KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,   XXXXXXX,
+     KC_LALT, KC_A, KC_S, KC_D,     KC_F,    KC_G,                                       KC_H,   KC_J,    KC_K,    KC_L,   KC_SCLN, KC_RALT,
+     XXXXXXX, KC_Z, KC_X, KC_C,     KC_V,    KC_B,  _______,  ADJUST,  ADJUST, _______,  KC_N,  KC_M,    KC_COMM, KC_DOT, KC_SLSH, XXXXXXX,
                           KC_MUTE, LCTL_T(KC_ESC), LOWER, KC_LSFT, _______, _______,  KC_SPC, RAISE, LGUI_T(KC_BSPC), _______
     ),
 
@@ -100,9 +100,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `--------------------------------'  `----------------------------------'
  */
     [_LOWER] = LAYOUT(
-      KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSPC,
- KC_GRV,KC_RGUI,OSM(MOD_RCTL),OSM(MOD_LALT),OSM(MOD_LSFT),OSM(MOD_RALT),                        KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_TILD, KC_PIPE,
-      KC_PSCR, KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5, _______, _______, _______, _______, KC_F6,   KC_GRV , KC_PIPE, KC_HOME, KC_END,  _______,
+      XXXXXXX, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                                     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, XXXXXXX,
+ XXXXXXX,KC_RGUI,OSM(MOD_RCTL),OSM(MOD_LALT),OSM(MOD_LSFT),OSM(MOD_RALT),                        KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, KC_TILD, XXXXXXX,
+      KC_PSCR, KC_F1,   KC_F2,    KC_F3,    KC_F4,   KC_F5, _______, _______, _______, _______, KC_F6,   KC_GRV , KC_PIPE, KC_HOME, KC_END,  XXXXXXX,
                                 _______, _______,  _______, _______, _______, _______,LCTL(KC_LSFT), _______, _______, _______
     ),
 
@@ -121,9 +121,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                      `----------------------------------'  `----------------------------------'
  */
     [_RAISE] = LAYOUT(
-      _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
-       KC_DEL, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,  KC_DEL,                                     KC_BSLS, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,
-      _______,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, _______, _______, _______, _______, KC_F12,  KC_QUOT, KC_NUBS, KC_PGDN, KC_PGUP, _______,
+      XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0, KC_BSPC,
+      XXXXXXX, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR,  KC_DEL,                                     KC_BSLS, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, XXXXXXX,
+      XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11, _______, _______, _______, _______, KC_F12,  KC_QUOT, KC_NUBS, KC_PGDN, KC_PGUP, XXXXXXX,
                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -142,10 +142,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                      `----------------------------------'  `----------------------------------'
  */
     [_WINDOW] = LAYOUT(
- KC_CAPS,LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),                                    LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9),LGUI(KC_0),_______,
- LGUI(LSFT(KC_E)),LGUI(LSFT(KC_Q)),LGUI(KC_SPC),OSM(MOD_LALT),OSM(MOD_LSFT),OSM(MOD_RALT),          LGUI(LSFT(KC_LEFT)),LGUI(LSFT(KC_DOWN)),LGUI(LSFT(KC_UP)),LGUI(LSFT(KC_RGHT)),LGUI(LSFT(KC_E)),_______,
-      _______, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, OSM(MOD_LCTL), _______, _______, _______, _______, ADJUST_HOLD, _______, KC_PSCR, KC_INS, KC_CAPS, _______,
-                                  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+ KC_CAPS,LGUI(KC_1),LGUI(KC_2),LGUI(KC_3),LGUI(KC_4),LGUI(KC_5),                                    LGUI(KC_6),LGUI(KC_7),LGUI(KC_8),LGUI(KC_9),LGUI(KC_0),XXXXXXX,
+ LGUI(LSFT(KC_E)),LGUI(LSFT(KC_Q)),LGUI(KC_SPC),OSM(MOD_LALT),OSM(MOD_LSFT),OSM(MOD_RALT),          LGUI(LSFT(KC_LEFT)),LGUI(LSFT(KC_DOWN)),LGUI(LSFT(KC_UP)),LGUI(LSFT(KC_RGHT)),LGUI(LSFT(KC_E)), XXXXXXX,
+      XXXXXXX, KC_BRID, KC_BRIU, KC_VOLD, KC_VOLU, OSM(MOD_LCTL), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, ADJUST_HOLD, XXXXXXX, KC_PSCR, KC_INS, KC_CAPS, XXXXXXX,
+                                  _______, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, _______
     ),
 
 /*
