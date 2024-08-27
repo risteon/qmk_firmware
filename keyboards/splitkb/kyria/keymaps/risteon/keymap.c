80d61c51ddd0550d4330cd58e9bde10c4765a571
Christoph Rist <c.rist@posteo.de>
2024-08-27 12:30:11 +0200

    WIP add tap dance

--- keyboards/splitkb/kyria/keymaps/risteon/keymap.c
+++ keyboards/splitkb/kyria/keymaps/risteon/keymap.c
@@ -587,3 +587,14 @@ void keyboard_post_init_user(void) {
 //     }
 // }
 #endif // RGBLIGHT_LAYER_BLINK
+
+// Tap Dance declarations
+enum {
+    TD_ESC_CAPS,
+};
+
+// Tap Dance definitions
+tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_ESC_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, KC_CAPS),
+};
